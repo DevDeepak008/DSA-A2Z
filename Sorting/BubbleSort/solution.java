56688beec3793230c00094bf3143217de9ed1a0a
DevDeepak008 <thedevdeepak@gmail.com>
2026-01-04 21:45:50 +0530

    Sorting Algorithms added

---
+++ Sorting/BubbleSort/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] bubbleSort(int[] nums) {
+        int n = nums.length;
+        // Traverse through the array
+        for (int i = n - 1; i >= 0; i--) {
+            // Track if swaps are made
+            boolean isSwapped = false;
+            for (int j = 0; j <= i - 1; j++) {
+                // Swap if next element is smaller
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                    isSwapped = true;
+                }
+            }
+            /**
+             * Break out of loop
+             * if no swaps done
+             */
+            if (!isSwapped) {
+                break;
+            }
+        }
+        return nums;
+    }
+}
