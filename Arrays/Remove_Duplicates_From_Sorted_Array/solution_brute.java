59991bfb280009c50ab06dece3b3db12d112f723
DevDeepak008 <thedevdeepak@gmail.com>
2026-01-07 23:06:02 +0530

    remove duplicates from sorted array

---
+++ Arrays/Remove_Duplicates_From_Sorted_Array/solution_brute.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        // TreeSet to store unique elements in sorted order
+        Set<Integer> s = new TreeSet<>();
+
+        // Add all elements from array to the Set
+        for (int val : nums) {
+            s.add(val);
+        }
+
+        // Get the number of unique elements
+        int k = s.size();
+
+        int j = 0;
+        // Copy unique elements from set to array
+        for (int val : s) {
+            nums[j++] = val;
+        }
+
+        // Return the number of unique elements
+        return k;
+    }
+}
