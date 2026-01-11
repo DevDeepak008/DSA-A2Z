81d9d82a8bcea1b5c8a6d0706841ac80b73e6718
DevDeepak008 <thedevdeepak@gmail.com>
2026-01-11 22:42:27 +0530

    pascal triangle III

---
+++ Arrays/Pascal_Triangle_III/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> pascalTriangleIII(int n) {
+        List<List<Integer>> pascalTriangle = new ArrayList<>();
+
+        // Compute the entire Pascal's Triangle
+        for(int row = 1; row <= n; row++) {
+            pascalTriangle.add(generateRow(row));
+        }
+
+        // return the pascalTriangle
+        return pascalTriangle;
+    }
+
+    private List<Integer> generateRow(int row) {
+        long ans = 1;
+        List<Integer> ansRow = new ArrayList<>();
+
+        // Inserting the 1st element
+        ansRow.add(1);
+
+        // Calculate the rest of the elements
+        for(int col = 1; col < row; col++) {
+            ans = ans * (row - col);
+            ans = ans / col;
+            ansRow.add((int) ans);
+        }
+
+        return ansRow; // Return the computed row
+    }
+}
