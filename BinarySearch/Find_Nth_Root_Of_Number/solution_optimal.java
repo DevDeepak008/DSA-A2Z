18f4a111800a78fdc94a6a5fc776c63acda2dbfa
DevDeepak008 <thedevdeepak@gmail.com>
2026-02-09 20:20:46 +0530

    find nth root of a number

---
+++ BinarySearch/Find_Nth_Root_Of_Number/solution_optimal.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int NthRoot(int N, int M) {
+        int low = 1, high = M;
+
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            int midN = helperFunc(mid, N, M);
+
+            if(midN == 1) return mid; // Found exact root
+            else if(midN == 0) low = mid + 1; // Move right
+            else high = mid - 1; // Move left
+        }
+        return -1; // No integer root found
+    }
+
+    // Helper Function to check mid^N compared to M
+    private int helperFunc(int mid, int n, int m) {
+        long ans = 1, base = mid;
+
+        while(n > 0) {
+            if(n % 2 == 1) {
+                ans *= base;
+                if(ans > m) return 2; // Early exit
+                n--;
+            } else {
+                n /= 2;
+                base *= base;
+                if(base > m) return 2;
+            }
+        }
+        if(ans == m) return 1;
+        return 0;
+    }
+}
