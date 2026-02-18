7627588651887863782017886d54a356f346286c
DevDeepak008 <thedevdeepak@gmail.com>
2026-02-18 22:57:19 +0530

    minimize max distance to gas station

---
+++ BinarySearch/Minimize_Max_Distance_To_Gas_Station/solution_better.java
@@ -0,0 +1,28 @@
+class Solution {
+    public double minimiseMaxDistance(int[] arr, int k) {
+        int n = arr.length;
+        int[] howMany = new int[n - 1];
+
+        // Max heap of double[]: [distance, sectionIndex]
+        PriorityQueue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
+
+        for (int i = 0; i < n - 1; i++) {
+            double dist = arr[i + 1] - arr[i];
+            pq.offer(new double[]{dist, i});
+        }
+
+        for (int gasStations = 1; gasStations <= k; gasStations++) {
+            double[] top = pq.poll();
+
+            int secInd = (int) top[1];
+            howMany[secInd]++;
+
+            double initialDist = arr[secInd + 1] - arr[secInd];
+            double newSecLen = initialDist / (howMany[secInd] + 1);
+
+            pq.offer(new double[]{newSecLen, secInd});
+        }
+
+        return pq.peek()[0];
+    }
+}
