283ceabfb8d5a2157aa8baeb714eed5a7380c60e
DevDeepak008 <thedevdeepak@gmail.com>
2026-02-25 23:36:40 +0530

    delete of the kth element of LL

---
+++ LinkedList/Delete_Of_The_Kth_Element_Of_LL/solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    // Function to delete the k-th node of a linked list
+    public ListNode deleteKthNode(ListNode head, int k) {
+        // If the list is empty, return null
+        if (head == null)
+            return null;
+
+        // If k is 1, delete the head node
+        if (k == 1) {
+            ListNode temp = head;
+            head = head.next;
+            return head;
+        }
+
+        // Initialize a temporary pointer
+        ListNode temp = head;
+
+        // Traverse to the (k-1)th node
+        for (int i = 0; temp != null && i < k - 2; i++) {
+            temp = temp.next;
+        }
+
+        /* If k is greater than the number of nodes, 
+        return the unchanged list */
+        if (temp == null || temp.next == null)
+            return head;
+
+        // Delete the k-th node
+        ListNode next = temp.next.next;
+        temp.next = next;
+
+        // Return head
+        return head;
+    }
+}
