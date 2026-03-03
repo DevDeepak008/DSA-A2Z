ea68c60311de72eff63c1145e848316d84acf3cc
DevDeepak008 <thedevdeepak@gmail.com>
2026-03-03 22:22:38 +0530

    insert before given node in doubly LL

---
+++ LinkedList/Insert_Before_Given_Node_In_Doubly_LL/solution.java
@@ -0,0 +1,19 @@
+// Solution class
+class Solution {
+    /* Function to insert a new node before
+    given node in a doubly linked list */
+    public void insertBeforeGivenNode(ListNode node, int X) {
+        // Get node before the given node
+        ListNode prev = node.prev;
+
+        // Create new node
+        ListNode newNode = new ListNode(X, prev, node);
+
+        // Connect newNode
+        prev.next = newNode;
+        node.prev = newNode;
+
+        // void function to just update
+        return;
+    }
+}
