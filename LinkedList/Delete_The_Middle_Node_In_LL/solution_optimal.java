943e5a431fddf50490eb0f70084e33caf6117674
DevDeepak008 <thedevdeepak@gmail.com>
2026-03-11 23:24:18 +0530

    delete the middle node in LL

---
+++ LinkedList/Delete_The_Middle_Node_In_LL/solution_optimal.java
@@ -0,0 +1,24 @@
+class Solution {
+    // Function to delete the middle node of a linked list
+    public ListNode deleteMiddle(ListNode head) {
+        /* If the list is empty or has only one node,
+         * return null as there is no middle node to delete */
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        // Initialize slow and fast pointers
+        ListNode slow = head;
+        ListNode fast = head.next.next;
+
+        // Move 'fast' pointer twice as fast as 'slow'
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Delete the middle node by skipping it
+        slow.next = slow.next.next;
+        return head;
+    }
+}
