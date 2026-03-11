9fb975361a6942c448cd141a26d705e3646e0030
DevDeepak008 <thedevdeepak@gmail.com>
2026-03-11 23:22:47 +0530

    delete the middle node in LL

---
+++ LinkedList/Delete_The_Middle_Node_In_LL/solution_brute.java
@@ -0,0 +1,52 @@
+class Solution {
+    // Function to delete middle node of linked list
+    public ListNode deleteMiddle(ListNode head) {
+        /* Edge case: if the list is empty 
+         * or has only one node, return null */
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        // Temporary node to traverse
+        ListNode temp = head;
+        
+        // Variable to store number of nodes
+        int n = 0;
+        
+        /* Loop to count the number of nodes 
+        in the linked list */
+        while (temp != null) {
+            n++;
+            temp = temp.next;
+        }
+        
+        // Index of the middle node
+        int middleIndex = n / 2;
+        
+        // Reset temporary node 
+        // to beginning of linked list
+        temp = head;
+        
+        /* Loop to find the node 
+        just before the middle node */
+        for (int i = 1; i < middleIndex; i++) {
+            temp = temp.next;
+        }
+        
+        // If the middle node is found
+        if (temp.next != null) {
+            // Create pointer to the middle node
+            ListNode middle = temp.next;
+            
+            // Adjust pointers to skip middle node
+            temp.next = temp.next.next;
+            
+            /* Free the memory allocated 
+             * to the middle node */
+            middle = null;
+        }
+        
+        // Return head
+        return head;
+    }
+}
