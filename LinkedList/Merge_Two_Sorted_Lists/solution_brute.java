af36ca0f6f8e670a6f8f717db1b27c1e156d2c8a
DevDeepak008 <thedevdeepak@gmail.com>
2026-03-19 22:14:54 +0530

    merge two sorted lists

---
+++ LinkedList/Merge_Two_Sorted_Lists/solution_brute.java
@@ -0,0 +1,37 @@
+class Solution {
+    // Function to merge two sorted linked lists
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        ListNode temp1 = list1;
+        ListNode temp2 = list2;
+
+        // Add elements from list1 to the vector
+        while (temp1 != null) {
+            arr.add(temp1.val);
+            // Move to the next node in list1
+            temp1 = temp1.next;
+        }
+
+        // Add elements from list2 to the vector
+        while (temp2 != null) {
+            arr.add(temp2.val);
+            // Move to the next node in list2
+            temp2 = temp2.next;
+        }
+
+        // Sorting the vector in ascending order
+        Collections.sort(arr);
+
+        // Sorted vector to linked list
+        ListNode dummyNode = new ListNode(-1);
+        ListNode temp = dummyNode;
+        for (int i = 0; i < arr.size(); i++) {
+            temp.next = new ListNode(arr.get(i));
+            temp = temp.next;
+        }
+
+        // Return the head of 
+        // merged sorted linked list
+        return dummyNode.next;
+    }
+}
