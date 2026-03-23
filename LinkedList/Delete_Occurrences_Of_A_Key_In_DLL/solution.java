13b0d731146825ed409e56bb87cf37adf74f6d4c
DevDeepak008 <thedevdeepak@gmail.com>
2026-03-23 22:19:24 +0530

    delete occurrences of a key in DLL

---
+++ LinkedList/Delete_Occurrences_Of_A_Key_In_DLL/solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    // Function to delete all occurrences of a target value
+    public ListNode deleteAllOccurrences(ListNode head, int target) {
+        ListNode temp = head;
+
+        while (temp != null) {
+            if (temp.val == target) {
+                // Update head if needed
+                if (temp == head) {
+                    head = temp.next;
+                }
+
+                ListNode nextNode = temp.next;
+                ListNode prevNode = temp.prev;
+
+                // Update next node's previous
+                if (nextNode != null) {
+                    nextNode.prev = prevNode;
+                }
+
+                // Update previous node's next
+                if (prevNode != null) {
+                    prevNode.next = nextNode;
+                }
+
+                // Delete the current node
+                temp = nextNode;
+            } else {
+                temp = temp.next;
+            }
+        }
+
+        return head;
+    }
+}
