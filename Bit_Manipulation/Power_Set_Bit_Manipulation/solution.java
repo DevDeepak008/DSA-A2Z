ad979ad285632eb4328e4d71be2b103541cfff19
DevDeepak008 <thedevdeepak@gmail.com>
2026-03-31 22:41:41 +0530

    power set bit manipulation

---
+++ Bit_Manipulation/Power_Set_Bit_Manipulation/solution.java
@@ -0,0 +1,37 @@
+class Solution {
+    /* Function call to get the
+    Power set of given array */
+    public List<List<Integer>> powerSet(int[] nums) {
+        
+        // Variable to store size of array
+        int n = nums.length;
+        
+        // To store the answer
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        /* Variable to store the 
+        count of total susbsets */
+        int count = (1 << n);
+        
+        // Traverse for every value
+        for (int val = 0; val < count; val++) {
+            
+            // To store the current subset
+            List<Integer> subset = new ArrayList<>();
+            
+            // Traverse on n bits
+            for (int i = 0; i < n; i++) {
+                if ((val & (1 << i)) != 0) {
+                    subset.add(nums[i]);
+                }
+            }
+            
+            /* Add the current subset 
+            to final answer */
+            ans.add(subset);
+        }
+        
+        // Return stored answer
+        return ans;
+    }
+}
